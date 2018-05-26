685f75ebf08991eb66088e85f56d795cda440f8b
Martijn Loth <mtijn.loth@gmail.com>
2018-05-26 10:34:21 +0200

    Clean ip

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,8 @@
-console.log( 'Hi there' );
-
 let lol = () => {
-	console.log( 'This is fun 😎!' );
+	console.log( 'This is funny 😎!' );
 }
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson
 
 lol();
+
+console.log( 'Not working...' );
